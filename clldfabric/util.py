ed88685780612badf940128f8bd7772ba1240344
xrotwang <xrotwang@googlemail.com>
2016-01-08 09:29:54 +0100

    upgrade to work with clld >= 2.x

--- clldfabric/util.py
+++ clldfabric/util.py
@@ -359,6 +359,9 @@ def deploy(app, environment, with_alembic=False, with_blog=False, with_files=Tru
                         sudo('sudo -u postgres vacuumdb -z -d %s' % app.name)
 
     template_variables['TEST'] = {'test': True, 'production': False}[environment]
+    template_variables['files'] = False
+    if exists(app.www.joinpath('files')):
+        template_variables['files'] = app.www.joinpath('files')
     upload_template_as_root(app.config, 'config.ini', template_variables)
     upload_template_as_root(app.newrelic_config, 'newrelic.ini', template_variables)
 
